ff095fe3a7542d1e6943f0871648aabef1564e71
Jean Chalret <jecdr_75@hotmail.fr>
2021-09-08 11:32:20 +0200

    amélioration tests

--- backend/tests/test.js
+++ backend/tests/test.js
@@ -89,7 +89,6 @@ describe("Tests API", function () {
       .attach("image", __dirname + "/test-image.jpg")
       .auth(auth.token, { type: "bearer" });
     suppFile(response.body.sauce);
-    //--------------------------------------------
     response = await request(app).get("/api/sauces");
     expect(response.statusCode).toBe(200);
     expect(response.body.length).toBe(2);
