3462557ca3e665a18141a7df78f4011ddc151076
Jean Chalret <jecdr_75@hotmail.fr>
2021-09-14 11:56:52 +0200

    modif routes Sauces

--- backend/routes/routesUsers.js
+++ backend/routes/routesUsers.js
@@ -4,9 +4,5 @@ const controlUsers = require("../controllers/controlUsers.js");
 
 router.post("/signup", controlUsers.signup);
 router.post("/login", controlUsers.login);
-// router.put("/:id", controlUsers.modifyUser);
-// router.delete("/:id", controlUsers.deleteUser);
-// router.get("/:id", controlUsers.getOneUser);
-// router.get("/", controlUsers.getAllUsers);
 
 module.exports = router;
